e65b1c35cd3f2a7cd56c1d8f752205064a3c52ac
Emily Cotter <emily.g.cotter@gmail.com>
2017-10-25 19:02:44 -0400

    Delete coffee.js

--- coffee.js
+++
@@ -1,6 +0,0 @@
-var userCups = prompt("How many cups of coffee have you had today?")
-if (userCups < 3) {
-  console.log("Yes I'll take another cup!!");
-} else {
-  console.log("I think I'm okay for now.");
-}
